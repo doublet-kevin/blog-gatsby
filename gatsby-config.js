3fe6015c7eb2426fb19617b279e6c72f5a42df05
SCFloki <kevin.doublet02@gmail.com>
2023-07-04 14:17:12 +0200

    env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,60 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+    apiURL: process.env.STRAPI_API_URL,
+    accessToken: process.env.STRAPI_TOKEN,
+    collectionTypes: [
+        {
+            singularName: "article",
+            queryParams: {
+                publicationState: process.env.GATSBY_IS_PREVIEW === "true" ? "preview" : "live",
+                populate: {
+                    category: { populate: "*" },
+                    cover: "*",
+                    blocks: {
+                        populate: "*",
+                    },
+                },
+            },
+        },
+    ],
+    singleTypes: [],
+    remoteFileHeaders: {
+        /**
+         * Customized request headers
+         * For http request with a image or other files need authorization
+         * For expamle: Fetch a CDN file which has a security config when gatsby building needs
+         */
+        Referer: "https://your-site-domain/",
+        // Authorization: "Bearer eyJhabcdefg_replace_it_with_your_own_token",
+    },
+};
+
 module.exports = {
     siteMetadata: {
         title: `gatsby`,
         siteUrl: `https://www.yourdomain.tld`
     },
-    plugins: ["gatsby-plugin-mdx", {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "pages",
-            "path": "./src/pages/"
+    plugins: [
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-postcss',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
         },
-        __key: "pages"
-    },'gatsby-plugin-postcss']
+        {
+            resolve: `gatsby-source-strapi`,
+            options: strapiConfig,
+        },
+    ]
 };
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <section className="bg-neutral-900">
+            <div className="container grid grid-cols-4 gap-8 lg:gap-12">
+                <div className="col-span-1">Bonjour</div>
+                <div className="col-span-1">Bonjour</div>
+                <div className="col-span-1">Bonjour</div>
+                <div className="col-span-1">Bonjour</div>
+            </div>
+        </section>
+    );
+}
+
+export default Footer;
